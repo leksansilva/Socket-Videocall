7f735ec4ee135ff2bf0b1c89b5e85a906a579383
leksansilva <69767375+leksansilva@users.noreply.github.com>
2022-08-04 23:13:13 -0300

    teste 90589438

--- web/src/environments/environment.prod.ts
+++ web/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   peer: {
-    host: 'https://socket-videocall-production.up.railway.app',
+    host: 'socket-videocall-production.up.railway.app',
     path: '/peer',
   },
   socket: 'https://socket-videocall-production.up.railway.app',
